1c13f1b46620e2d1ed53ace1721fbd52a7fa33e2
Kausar Hina <kausar.hina@gmail.com>
2024-06-10 11:25:10 -0600

    Week 4 Workshop Assignment

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -14,6 +14,7 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -63,6 +64,8 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
+
 
 
 
